a8fa11370c853272c626543593c3ec414390c69d
Mathriok <32360968+Mathriok@users.noreply.github.com>
2018-12-04 12:02:54 +0100

    lunchcommit

--- src/videopoker/VideoPoker.java
+++ src/videopoker/VideoPoker.java
@@ -1,10 +1,26 @@
 package videopoker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VideoPoker {
 	
 	/*	This class will contain the games intelligence.
 	 * 	This will be epic
 	 * 
 	 */
-
-}
+	
+	public boolean checkForPairs(int a, int b, List<Card> hand) {
+//kolla par + par || par + tretal
+		Deck deck = new Deck();
+		deck.shuffle();
+		hand.add(deck.draw());
+		hand.add(deck.draw());
+		hand.add(deck.draw());
+		hand.add(deck.draw());
+		hand.add(deck.draw());
+		
+		return false;
+		
+	}
+ }
