a882d7d93a79cb86526c0e5296d7a32e2dd3237c
Johan Cederschiold <johan.cederschiold@gmail.com>
2018-12-04 11:24:13 +0100

    Added sort method to Videopoker

--- src/videopoker/VideoPoker.java
+++ src/videopoker/VideoPoker.java
@@ -1,5 +1,7 @@
 package videopoker;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class VideoPoker {
@@ -11,9 +13,22 @@ public class VideoPoker {
 	
 	
 	
-//	public List<Card> sortCards (List<Card> handToSort) {
-//		
-//	}
+	public List<Card> sortCards (List<Card> handToSort) {
+		
+		List<Card> workThis = handToSort;
+		
+		for (int i = 0 ; i < handToSort.size(); i++ ) {
+			for (int j = 0 ; j < handToSort.size() - 1; j++ ) {
+				if (calculateValueOfCard(handToSort.get(j)) >  calculateValueOfCard(handToSort.get(j + 1)) ) {
+					Card temp = handToSort.get(j);handToSort.remove(j);
+					handToSort.add(j + 1, temp);
+				}
+			}
+		}
+		
+		return handToSort;
+	
+	}
 	
 	public int calculateValueOfCard(Card card) {
 		
