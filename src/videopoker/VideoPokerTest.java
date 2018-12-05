d2c4bc2e8839fbfd46c24bf1dae379a5fa7f0c37
Johan Cederschiold <johan.cederschiold@gmail.com>
2018-12-05 10:36:04 +0100

    Added testmethods for fullhouse and two pairs

--- src/videopoker/VideoPokerTest.java
+++ src/videopoker/VideoPokerTest.java
@@ -1,5 +1,5 @@
 package videopoker;
-i
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -92,7 +92,70 @@ public class VideoPokerTest {
 		hand.add(new Card(Suit.CLUBS, "Six"));
 		
 		assertFalse(video.CheckForStraight(hand));	
+
+	}
+	
+	@Test
+	void testForFullHouse() {
 		
+		VideoPoker video = new VideoPoker();
+		
+		List<Card> hand = new ArrayList<>();
+		hand.add(new Card(Suit.HEARTS, "Two"));
+		hand.add(new Card(Suit.SPADES, "Three"));
+		hand.add(new Card(Suit.DIAMONDS, "Three"));
+		hand.add(new Card(Suit.SPADES, "Two"));
+		hand.add(new Card(Suit.CLUBS, "Three"));
+		
+		assertTrue(video.checkForPairs(3, 2, hand));
+	
+	}
+	
+	@Test
+	void testForFullHouseFalse() {
+		
+		VideoPoker video = new VideoPoker();
+		
+		List<Card> hand = new ArrayList<>();
+		hand.add(new Card(Suit.HEARTS, "Two"));
+		hand.add(new Card(Suit.SPADES, "Four"));
+		hand.add(new Card(Suit.DIAMONDS, "Three"));
+		hand.add(new Card(Suit.SPADES, "Two"));
+		hand.add(new Card(Suit.CLUBS, "Three"));
+		
+		assertFalse(video.checkForPairs(3, 2, hand));
+	
+	}
+	
+	@Test
+	void testForTwoPairs() {
+		
+		VideoPoker video = new VideoPoker();
+		
+		List<Card> hand = new ArrayList<>();
+		hand.add(new Card(Suit.HEARTS, "Two"));
+		hand.add(new Card(Suit.SPADES, "Five"));
+		hand.add(new Card(Suit.DIAMONDS, "Three"));
+		hand.add(new Card(Suit.SPADES, "Two"));
+		hand.add(new Card(Suit.CLUBS, "Three"));
+		
+		assertTrue(video.checkForPairs(2, 2, hand));
+		
+	}
+	
+	@Test
+	void testForTwoPairFalse() {
+		
+		VideoPoker video = new VideoPoker();
+		
+		List<Card> hand = new ArrayList<>();
+		hand.add(new Card(Suit.HEARTS, "Two"));
+		hand.add(new Card(Suit.SPADES, "Five"));
+		hand.add(new Card(Suit.DIAMONDS, "Four"));
+		hand.add(new Card(Suit.SPADES, "Two"));
+		hand.add(new Card(Suit.CLUBS, "Three"));
+		
+		assertFalse(video.checkForPairs(2, 2, hand));
 		
 	}
 	
