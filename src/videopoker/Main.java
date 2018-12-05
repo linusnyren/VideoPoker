c123247f5ea853769122ccf6d920208338df684b
Mathriok <32360968+Mathriok@users.noreply.github.com>
2018-12-05 09:11:49 +0100

    CheckForPairs-commit

--- src/videopoker/Main.java
+++ src/videopoker/Main.java
@@ -6,33 +6,6 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-//		VideoPoker vp = new VideoPoker();
-//		List<Card> hand = new ArrayList<Card>();
-//		vp.checkForPairs(2, 2, hand);
-		
-		List<Card> handtest = new ArrayList<Card>();
-		Deck deck = new Deck();
-		handtest.add(deck.draw());
-		handtest.add(deck.draw());
-		handtest.add(deck.draw());
-		handtest.add(deck.draw());
-		handtest.add(deck.draw());
 
-		System.out.println(handtest.size() - 1);
-		System.out.println(handtest.get(0));
-		System.out.println(handtest.get(1));
-		System.out.println(handtest.get(2));
-		System.out.println(handtest.get(3));
-		System.out.println(handtest.get(4));
-
-		for (int j = 0; j < handtest.size()-1; j++) {
-			for (int i = 0; i < handtest.size()-1; i++) {
-				if(handtest.get(i).getRank() == handtest.get(j).getRank()) {
-				System.out.println("Par!");
-				System.out.println(handtest.get(j).getRank());
-				System.out.println(handtest.get(i).getRank());
-				}
-			}
-		}
 	}
 }
--- src/videopoker/VideoPoker.java
+++ src/videopoker/VideoPoker.java
@@ -5,82 +5,138 @@ import java.util.Collections;
 import java.util.List;
 
 public class VideoPoker {
-	
-	/*	This class will contain the games intelligence.
-	 * 	This will be epic
+
+	/*
+	 * This class will contain the games intelligence. This will be epic
 	 * 
 	 */
-	
 
 	public boolean checkForPairs(int a, int b, List<Card> hand) {
-//kolla par + par || par + tretal
-		Deck deck = new Deck();
-		deck.shuffle();
-		hand.add(deck.draw());
-		hand.add(deck.draw());
-		hand.add(deck.draw());
-		hand.add(deck.draw());
-		hand.add(deck.draw());
+		boolean par = false,tvåpar = false ,tretal = false;
+		sortCards(hand);
+		//Par
+		if 	(hand.get(0).getRank().equals(hand.get(1).getRank()) ||
+			hand.get(1).getRank().equals(hand.get(2).getRank())  ||
+			hand.get(2).getRank().equals(hand.get(3).getRank())  ||
+			hand.get(3).getRank().equals(hand.get(4).getRank()))
+		{
+		par = true;
+		}
+		else {
+		par = false;
+		}
+
+		//Två par
+		if (hand.get(0).getRank().equals(hand.get(1).getRank()) &&
+			hand.get(2).getRank().equals(hand.get(3).getRank())
+			||
+			hand.get(0).getRank().equals(hand.get(1).getRank()) &&
+			hand.get(3).getRank().equals(hand.get(4).getRank())
+			||
+			hand.get(1).getRank().equals(hand.get(2).getRank()) &&
+			hand.get(3).getRank().equals(hand.get(4).getRank()))	
+		{
+		tvåpar=true;	
+		}
 		
-		return false;
-		
+		 else {
+			tvåpar=false;
+		}
+
+	//Tre-tal
+		if (	hand.get(0).getRank().equals(hand.get(1).getRank()) &&
+				hand.get(1).getRank().equals(hand.get(2).getRank()) &&
+				!hand.get(4).getRank().equals(hand.get(3).getRank())&&
+				!hand.get(4).getRank().equals(hand.get(2).getRank())&&
+				!hand.get(3).getRank().equals(hand.get(2).getRank()) 
+				||
+				hand.get(1).getRank().equals(hand.get(2).getRank()) &&
+				hand.get(2).getRank().equals(hand.get(3).getRank()) &&
+				!hand.get(0).getRank().equals(hand.get(1).getRank())&&
+				!hand.get(0).getRank().equals(hand.get(4).getRank())&&
+				!hand.get(3).getRank().equals(hand.get(4).getRank())
+				||
+				hand.get(2).getRank().equals(hand.get(3).getRank()) &&
+				hand.get(3).getRank().equals(hand.get(4).getRank()) &&
+				!hand.get(0).getRank().equals(hand.get(1).getRank())&&
+				!hand.get(0).getRank().equals(hand.get(2).getRank())&&
+				!hand.get(4).getRank().equals(hand.get(1).getRank()))
+		{
+			tretal = true;
+		}
+		else {
+			tretal = false;
+		}
+//			Kontrollerar tvåpar						Kontrollerar kåk											Kontrollerar kåk
+		if(a == 2 && b == 2 && tvåpar == true || a == 2 && b == 3 && par == true && tretal == true || a == 3 && b == 2 && par == true && tretal == true)
+	 {
+		return true;
 	}
-	public List<Card> sortCards (List<Card> handToSort) {
-		
+	else {
+		return false;
+	}
+	}
+
+	public List<Card> sortCards(List<Card> handToSort) {
+
 		List<Card> workThis = handToSort;
-		
-		for (int i = 0 ; i < handToSort.size(); i++ ) {
-			for (int j = 0 ; j < handToSort.size() - 1; j++ ) {
-				if (calculateValueOfCard(handToSort.get(j)) >  calculateValueOfCard(handToSort.get(j + 1)) ) {
-					Card temp = handToSort.get(j);handToSort.remove(j);
+
+		for (int i = 0; i < handToSort.size(); i++) {
+			for (int j = 0; j < handToSort.size() - 1; j++) {
+				if (calculateValueOfCard(handToSort.get(j)) > calculateValueOfCard(handToSort.get(j + 1))) {
+					Card temp = handToSort.get(j);
+					handToSort.remove(j);
 					handToSort.add(j + 1, temp);
 				}
 			}
 		}
-		
+
 		return handToSort;
-	
+
 	}
+
 	public boolean CheckForEquals(int par, List<Card> test) {
-		
-		if ( par == 3) {
-			if (calculateValueOfCard(test.get(0)) == calculateValueOfCard(test.get(1)) && calculateValueOfCard(test.get(1)) == calculateValueOfCard(test.get(2))){
-			return true;
+
+		if (par == 3) {
+			if (calculateValueOfCard(test.get(0)) == calculateValueOfCard(test.get(1))
+					&& calculateValueOfCard(test.get(1)) == calculateValueOfCard(test.get(2))) {
+				return true;
 			}
 		}
-		
+
 		else if (par == 2) {
 			if (calculateValueOfCard(test.get(0)) == calculateValueOfCard(test.get(1))) {
-			return true;
+				return true;
 			}
 		}
 		return false;
-		
+
 	}
+
 	public boolean CheckForStraight(List<Card> test) {
 		int a = calculateValueOfCard(test.get(0));
 		int b = calculateValueOfCard(test.get(1));
 		int c = calculateValueOfCard(test.get(2));
 		int d = calculateValueOfCard(test.get(3));
 		int e = calculateValueOfCard(test.get(4));
-		if (a+1 == b && b+1 == c && c+1 == d && d+1 == e)
+		if (a + 1 == b && b + 1 == c && c + 1 == d && d + 1 == e)
 			return true;
-		
+
 		else
 			return false;
 	}
-	
+
 	public int calculateValueOfCard(Card card) {
-		
+
 		int value = 2;
-		
+
 		for (String rank : Deck.getRanks()) {
 			if (card.getRank().equals(rank)) {
 				return value;
 			}
 			value++;
 		}
-		
+
 		return -1;
 	}
 }
